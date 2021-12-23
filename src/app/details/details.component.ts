f9e0b1820f8671fe8351dab8a761091855a7d8e1
kanojiyamahesh8 <83824288+kanojiyamahesh8@users.noreply.github.com>
2021-12-23 20:33:31 +0530

    details page made + able to fetch hardcoded data

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
+  data = { "name":"Harry Potter","alternate_names":[""],"species":"human","gender":"male","house":"Gryffindor","dateOfBirth":"31-07-1980","yearOfBirth":1980,"wizard":true,"ancestry":"half-blood","eyeColour":"green","hairColour":"black","wand":{"wood":"holly","core":"phoenix feather","length":11},"patronus":"stag","hogwartsStudent":true,"hogwartsStaff":false,"actor":"Daniel Radcliffe","alternate_actors":[""],"alive":true,"image":"http://hp-api.herokuapp.com/images/harry.jpg"};
+  data_of_single_wizard = { "name":"Harry Potter","alternate_names":[""],"species":"human","gender":"male","house":"Gryffindor","dateOfBirth":"31-07-1980","yearOfBirth":1980,"wizard":true,"ancestry":"half-blood","eyeColour":"green","hairColour":"black","wand":{"wood":"holly","core":"phoenix feather","length":11},"patronus":"stag","hogwartsStudent":true,"hogwartsStaff":false,"actor":"Daniel Radcliffe","alternate_actors":[""],"alive":true,"image":"http://hp-api.herokuapp.com/images/harry.jpg"};
+
 
   constructor() { }
 
